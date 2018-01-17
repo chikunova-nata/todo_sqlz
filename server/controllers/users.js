800128e09b1a5ee1184fe839d6855f0d1b027880
chikunova-nata <chikunova.nata@gmail.com>
2018-01-17 23:42:40 +0200

    user model unit tests

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -4,6 +4,10 @@ const jwt = require('jsonwebtoken');
 
 module.exports = {
     create(req, res) {
+      if(!req.body.fullName || !req.body.email || !req.body.password) {
+        return response.status(401).send("Invalid credentials.");
+      } 
+
       return User
       .create({
         fullName: req.body.fullName,
@@ -11,7 +15,7 @@ module.exports = {
         password: req.body.password
       })
       .then(user => {
-        if (!user) return res.status(400).send("There was a problem registering the user.");
+        if (!user) return res.status(404).send('No user found.');
         
         const token = jwt.sign({ data: user.email }, config.secret, { expiresIn: '24h' });
         res.status(201).send({ token })
@@ -30,22 +34,25 @@ module.exports = {
     },
     authenticate(req, res) {
       const token = req.headers['x-access-token'];
-      if (!token) return res.status(401).send({ message: 'No token provided.' });
+      if (!token) return res.status(401).send({ message: 'An authorization header is required.' });
       
       jwt.verify(token, config.secret, function(err, decoded) {
-        if (err) return res.status(401).send({ message: 'Failed to authenticate token.' });   
+        if (err) return res.status(401).send({ message: 'Invalid authorization token.' });   
         res.status(200).send({ expires: decoded.exp});
       });
     },
     login(req, res) {
+      if(!req.query.email) {
+        return response.status(400).send("User's email is required.");
+      }
+
       return User
       .findOne({ where: {email: req.query.email} })
       .then(user => {
         if (!user) return res.status(400).send({message: 'User Not Found'});
           
         const token = jwt.sign({ data: user.email }, config.secret, { expiresIn: '24h' });
-        res.status(200).send({ token });
-        return res.status(200).send(user);
+        return res.status(200).send({ token });
       })
       .catch(error => res.status(400).send(error));
     },
--- server/tests/user.test.js
+++ server/tests/user.test.js
@@ -1,12 +1,60 @@
+process.env.NODE_ENV = 'test';
+const API_BASE = '127.0.0.1:8000';
+const userModel = require('../models').User;
+const faker = require('faker');
+const app = require('../../app');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
+chai.use(chaiHttp);
+
 describe('Account', function() {
 
+    const fakeUser = {
+        fullName: faker.name.findName(),
+        email: faker.internet.email(),
+        password: faker.internet.password()
+    }
+    const fakeEmail = 'nata5@gmail.com';
+    const fakeToken = 'bGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoibmF0YTVAZ21haWwuY29tIiwiaWF0IjoxNTE2MTM3NTYwLCJleHAiOjE1MTYyMjM5NjB9.zQtBKPtiQV7wBeWXPRXq_ZmouTB-rM1-GBwx7p6jenQ';
+
+    it('should retrieve the token', () => {
+        chai.request(app)
+        .get('/auth/login')
+        .query({email: fakeEmail})
+        .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            res.body.should.have.property('token').not.be('undefined');
+        });
+    });
+    
     it('should save new user account', () => {
+        chai.request(app)
+        .post('/auth/register')
+        .send(fakeUser)
+        .end((err, res) => {
+            res.status.should.equal(201);
+            res.type.should.equal('application/json');
+            res.body.should.have.property('token').not.be('undefined');
+            }
+        )
     });
     
     it('should correctly update an existing account by the given id', () => {
     });
 
-    it('should retrieve existing account by the given id when token is valid', () => {
+    it('should retrieve existing account when token is valid', () => {
+        chai.request(app)
+        .get('/api/profile')
+        .query({email: fakeEmail})
+        .set('x-access-token', fakeToken)
+        .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            res.body.should.have.property('fullName').not.be('undefined');
+            res.body.should.have.property('email').not.be('undefined');
+        });
     });
 
     it('should return error when trying to save duplicate email', () => {
@@ -16,9 +64,24 @@ describe('Account', function() {
     });
 
     it('should return 403 error for non authorized user', () => {
+        chai.request(app)
+        .get('/api/profile')
+        .query({email: fakeEmail})
+        .end((err, res) => {
+            err.should.have.status(403);
+            err.body.should.be.a('object');
+            err.body.should.have.property('message').eq('Failed to authenticate token.');
+        });
     });
 
-    it('should return 401 when token is invalid', () => {
+    it('should return 401 for protected routes when token is invalid', () => {
+        chai.request(app)
+        .get('/api/todos')
+        .end((err, res) => {
+            err.should.have.status(401);
+            err.body.should.be.a('object');
+            err.body.should.have.property('message').eq('Failed to authenticate token.');
+        });
     });
 
     
